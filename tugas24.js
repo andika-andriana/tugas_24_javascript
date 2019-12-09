a82a6bcbea34cd24a73887a13dc69810b802b025
Andika Andriana <andriana.mia1@gmail.com>
2019-12-09 11:44:22 +0700

    Tugas 24 Javascript
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas24.js
@@ -0,0 +1,12 @@
+const tugas24 = () => {
+  var deret = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+  console.log("Sebelumnya : ", deret);
+
+  let asc = deret.sort();
+  console.log("Ascending : ", asc);
+
+  let dsc = deret.reverse();
+  console.log("Descending : ", dsc);
+}
+
+tugas24();
